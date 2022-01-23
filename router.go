25e7588504957f6b3173b1e12161aac13784b072
Andres Alvarez <andres15alvarez@gmail.com>
2022-01-23 17:06:01 -0400

    Create router to fing handlers

---
+++ router.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+)
+
+type Router struct {
+	rules map[string]map[string]http.HandlerFunc
+}
+
+func NewRouter() *Router {
+	return &Router{
+		rules: make(map[string]map[string]http.HandlerFunc),
+	}
+}
+
+func (r *Router) FindHandler(method string, path string) (http.HandlerFunc, bool, bool) {
+	_, exist := r.rules[path]
+	handler, methodExist := r.rules[path][method]
+	return handler, methodExist, exist
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	handler, methodExist, exist := r.FindHandler(request.Method, request.URL.Path)
+	if !methodExist {
+		w.WriteHeader((http.StatusMethodNotAllowed))
+		return
+	}
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	handler(w, request)
+}
