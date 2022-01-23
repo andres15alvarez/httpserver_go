ea0a9fcdb08897b02e1ff57e5e013d404f09e6af
Andres Alvarez <andres15alvarez@gmail.com>
2022-01-23 17:09:06 -0400

    Create handlers to create and receive users

---
+++ handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func HandlerHome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Home{"Hello, World"})
+}
+
+func HandlerGetUsers(w http.ResponseWriter, r *http.Request) {
+	users := []User{
+		{
+			Name:  "John Doe",
+			Email: "johndoe@example.com",
+		},
+		{
+			Name:  "Bob",
+			Email: "bob@example.com",
+		},
+		{
+			Name:  "Alice",
+			Email: "alice@example.com",
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
