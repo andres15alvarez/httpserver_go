17bd440e737dde4747f5d2a7207242dc6f632f05
Andres Alvarez <andres15alvarez@gmail.com>
2022-06-15 11:25:49 -0400

    Use global slice of users to update when post a new one

--- handlers.go
+++ handlers.go
@@ -6,26 +6,27 @@ import (
 	"net/http"
 )
 
+var users []User = []User{
+	{
+		Name:  "John Doe",
+		Email: "johndoe@example.com",
+	},
+	{
+		Name:  "Bob",
+		Email: "bob@example.com",
+	},
+	{
+		Name:  "Alice",
+		Email: "alice@example.com",
+	},
+}
+
 func HandlerHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Home{"Hello, World"})
 }
 
 func HandlerGetUsers(w http.ResponseWriter, r *http.Request) {
-	users := []User{
-		{
-			Name:  "John Doe",
-			Email: "johndoe@example.com",
-		},
-		{
-			Name:  "Bob",
-			Email: "bob@example.com",
-		},
-		{
-			Name:  "Alice",
-			Email: "alice@example.com",
-		},
-	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(users)
 }
@@ -38,6 +39,7 @@ func HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Error: %v", err)
 		return
 	}
+	users = append(users, user)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(user)
