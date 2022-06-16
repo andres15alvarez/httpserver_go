f133faa1ce39e4469d939a82898b8d7f59e0dde0
Andres Alvarez <andres15alvarez@gmail.com>
2022-06-16 19:07:03 -0400

    Test database connection

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "os"
+import (
+	"context"
+	"log"
+	"os"
+)
 
 func main() {
+	InitEnv()
+	db := ConnectDB()
+	ctx := context.Background()
+	_, err := db.ExecContext(ctx, "SELECT 1")
+	if err != nil {
+		log.Fatal(err)
+	}
 	server := NewServer(":" + os.Getenv("PORT"))
 	server.Handle("GET", "/", server.AddMiddleware(HandlerHome, Logger()))
 	server.Handle("POST", "/user", server.AddMiddleware(HandlerCreateUser, CheckAuth(), Logger()))
