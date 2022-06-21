914209dd7a879c9d09a60e4c2113b5073ee1db3a
Andres Alvarez <andres15alvarez@gmail.com>
2022-06-21 09:50:54 -0400

    Add test for CheckPasswordHash

--- tests/crypto_test.go
+++ tests/crypto_test.go
@@ -6,10 +6,18 @@ import (
 	"github.com/andres15alvarez/go_http_server/utils"
 )
 
-func Test_hashPassword(t *testing.T) {
+func TestHashPassword(t *testing.T) {
 	password := "holahola"
 	_, err := utils.HashPassword(password)
 	if err != nil {
 		t.Errorf("want hashed password got %q", err.Error())
+	}
+}
+
+func TestCheckPassworHash(t *testing.T) {
+	password := "holahola"
+	hashedPassword := "$2a$14$YrUBnEALoP8ekZwT5iwUJe3euijN5JVYRanuxxIVrOy4/w0FRwriy"
+	if !utils.CheckPasswordHash(password, hashedPassword) {
+		t.Error("want true got false")
 	}
 }
